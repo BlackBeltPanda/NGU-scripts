80b88d3198c8be99711f320338ad7ce2f491f0e9
BlackBeltPanda <dev@pandacraft.org>
2020-05-10 19:32:01 -0400

    Add idle loop argument

--- Python/Scripts/challenge.py
+++ Python/Scripts/challenge.py
@@ -10,6 +10,7 @@ import coordinates as coords
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--challenge", required=True, type=int, help="select which challenge you wish to run (1-11")
 parser.add_argument("-t", "--times", required=True, type=int, help="number of times to run challenge")
+parser.add_argument("-i", "--idle", action='store_true', help="run idle loop after finishing running")
 args = parser.parse_args()
 w = Window()
 feature = Features()
@@ -21,12 +22,14 @@ print(f"Top left found at: {w.x}, {w.y}")
 
 c = Challenge()
 
-print(f"Running challenge #{args.challenge} {args.times} times")
+print(f"Running challenge #{Challenge.list()[args.challenge-1]} {args.times} times")
 for x in range(args.times):
     c.start_challenge(args.challenge)
 
-
-#while True:  # main loop
-#    feature.itopod_snipe(300)
-#    feature.pit()
-#    feature.gold_diggers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+if args.idle:
+  print("Engaging idling loop")
+  while True:  # main loop
+      feature.itopod_snipe(300)
+      feature.pit()
+      feature.gold_diggers([2, 3, 4, 9])
+else: print("Exiting")
